af906c8711e15246b69e7613195345b6bbd043ec
Duman <52251067+Dumankrg2019@users.noreply.github.com>
2025-12-01 00:04:43 +0600

    KS-1-1 created variables for shop

---
+++ src/main/kotlin/lesson_01/lesson1_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_01
+
+fun main() {
+    var countOfOrders: Int = 75
+    val orderThanksMessage: String = "Thank you for your purchase!"
+
+    println("current orders: $countOfOrders")
+    println(orderThanksMessage)
+}
