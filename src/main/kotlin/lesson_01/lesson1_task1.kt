40893b4dfe6f00e54b1317d5aaaa28c1bf6aac4a
Duman <52251067+Dumankrg2019@users.noreply.github.com>
2025-12-02 18:48:28 +0600

    KS-1-1 remove all prints

--- src/main/kotlin/lesson_01/lesson1_task1.kt
+++ src/main/kotlin/lesson_01/lesson1_task1.kt
@@ -3,7 +3,4 @@ package org.example.lesson_01
 fun main() {
     var countOfOrders: Int = 75
     val orderThanksMessage: String = "Thank you for your purchase!"
-
-    println("current orders: $countOfOrders")
-    println(orderThanksMessage)
 }
