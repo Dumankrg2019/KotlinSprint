4dd6caad551719804d058066afb75566a0c1a3ec
Duman <52251067+Dumankrg2019@users.noreply.github.com>
2025-12-03 01:55:09 +0600

    KS-1-5 refactor code

--- src/main/kotlin/lesson_01/Lesson1_task5.kt
+++ src/main/kotlin/lesson_01/Lesson1_task5.kt
@@ -1,11 +1,15 @@
 package org.example.lesson_01
 
+const val SECONDS_IN_MINUTE: Int = 60
+const val SECONDS_IN_HOUR: Int = 60 * SECONDS_IN_MINUTE
+
 fun main() {
+
     val secondsInSpace: Short = 6480
 
-    val hours: Int = secondsInSpace / 3600
-    val minutes: Int = (secondsInSpace % 3600) / 60
-    val seconds: Int = secondsInSpace % 60
+    val hours: Int = secondsInSpace / SECONDS_IN_HOUR
+    val minutes: Int = (secondsInSpace % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds: Int = secondsInSpace % SECONDS_IN_MINUTE
 
     val timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
